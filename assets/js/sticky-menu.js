2550c07d7926537d263648b0eb88b35af7d05085
Gregor Capuder <capuderg@gmail.com>
2016-07-07 11:13:20 +0200

    Refactor JS. Added basic flex styling.

--- assets/js/sticky-menu.js
+++ assets/js/sticky-menu.js
@@ -1,3 +1,5 @@
+/* global Modernizr */
+
 /**
  * Sticky menu.
  */
@@ -5,44 +7,102 @@
 define( ['jquery', 'underscore'], function ( $, _ ) {
 	'use strict';
 
-	// Flag var, null for the start before we test.
-	var adminBarHeight = $( 'body' ).hasClass( 'admin-bar' ) ? 32 : 0,
-		stickyOffset     = $( '.js-sticky-offset' ).offset().top,
-		bodyStickyClass  = 'sticky-navigation';
+	var config = {
+		adminBarHeight:       $( 'body' ).hasClass( 'admin-bar' ) ? 32 : 0,
+		bodyStickyClass:      'sticky-navigation',
+		stickyOffsetClass:    'js-sticky-offset',
+	};
 
-	// Events listeners, everything goes trough here.
-	$( 'body' ).on( 'update_sticky_state.pt', function () {
-		if ( $( 'body' ).hasClass( bodyStickyClass ) ) {
-			addStickyNavbar();
-			$( window).trigger( 'scroll.stickyNavbar' );
-		}
-		else {
-			removeStickyNavbar();
-		}
+	var StickyMenu = function() {
+
+		// Set the initial config.stickyOffsetClass class to the appropriate DOM element.
+		this.setStickyOffsetClass();
+
+		// Get the initial offset.
+		this.stickyOffset = this.getStickyMenuOffset();
+
+		// Register the Event listeners.
+		this.registerUpdateEventListener();
+		this.registerResizeEventListener();
+
+		// Trigger for the initialization.
+		$( window ).trigger( 'resize.ptStickyMenu' );
+	};
+
+	_.extend( StickyMenu.prototype, {
+		/**
+		 * Set the config.stickyOffsetClass class to the appropriate DOM element.
+		 */
+		setStickyOffsetClass: function () {
+			$( '.js-sticky-desktop-option' ).toggleClass( config.stickyOffsetClass, Modernizr.mq( '(min-width: 992px)' ) );
+			$( '.js-sticky-mobile-option' ).toggleClass( config.stickyOffsetClass, Modernizr.mq( '(max-width: 991px)' ) );
+		},
+
+		/**
+		 * Register the sticky menu update event listener. Everything goes though here.
+		 */
+		registerUpdateEventListener: function () {
+			$( 'body' ).on( 'update.ptStickyMenu', _.bind( function () {
+				if ( $( 'body' ).hasClass( config.bodyStickyClass ) ) {
+					this.addStickyNavbar();
+					$( window).trigger( 'scroll.ptStickyMenu' );
+				}
+				else {
+					this.removeStickyNavbar();
+				}
+			}, this ) );
+		},
+
+		/**
+		 * Display the sticky menu (add a class '.is-sticky-nav' to the body).
+		 */
+		addStickyNavbar: function () {
+			$( window ).on( 'scroll.ptStickyMenu', _.bind( _.throttle( function() {
+				$( 'body' ).toggleClass( 'is-sticky-nav', $( window ).scrollTop() > ( this.stickyOffset - config.adminBarHeight ) );
+			}, 20 ), this ) ); // Only trigered once every 20ms = 50 fps = very cool for performance.
+		},
+
+		/**
+		 * Remove the sticky menu (remove the class '.is-sticky-nav' from the body).
+		 */
+		removeStickyNavbar: function () {
+			$( window ).off( 'scroll.ptStickyMenu' );
+			$( 'body' ).removeClass( 'is-sticky-nav' );
+		},
+
+		/**
+		 * Get the sticky menu offset.
+		 */
+		getStickyMenuOffset: function () {
+			if ( 0 < $( '.' + config.stickyOffsetClass ).length ) {
+				return $( '.' + config.stickyOffsetClass ).offset().top;
+			}
+
+			return 0;
+		},
+
+		/**
+		 * Register the sticky menu window resize event listener.
+		 */
+		registerResizeEventListener: function () {
+			$( window ).on( 'resize.ptStickyMenu', _.bind( _.debounce( function() {
+				// Update sticky offset.
+				this.setStickyOffsetClass();
+				this.stickyOffset = this.getStickyMenuOffset();
+
+				// Turn on or off the sticky behaviour, depending if the <body> has class config.bodyStickyClass.
+				$( 'body' ).trigger( 'update.ptStickyMenu' );
+			}, 100 ), this ) );
+		},
+
+		/**
+		 * Remove the sticky menu (remove the class '.is-sticky-nav' from the body).
+		 */
+		getAdminBarHeight: function () {
+
+		},
 	} );
 
-	// Add sticky navbar events and classes.
-	var addStickyNavbar = function () {
-		$( window).on( 'scroll.stickyNavbar', _.throttle( function() {
-			$( 'body' ).toggleClass( 'is-sticky-nav', $( window ).scrollTop() > ( stickyOffset - adminBarHeight ) );
-		}, 20 ) ); // Only trigered once every 20ms = 50 fps = very cool for performance.
-	};
+	new StickyMenu();
 
-	// Cleanup for events and classes.
-	var removeStickyNavbar = function () {
-		$( window ).off( 'scroll.stickyNavbar' );
-		$( 'body' ).removeClass( 'is-sticky-nav' );
-	};
-
-	// Event listener on the window resizing.
-	$( window ).on( 'resize.stickyNavbar', _.debounce( function() {
-		// Update sticky offset.
-		stickyOffset = $( '.js-sticky-offset' ).offset().top;
-
-		// Turn on or off the sticky behaviour, depending if the <body> has class "sticky-navigation".
-		$( 'body' ).trigger( 'update_sticky_state.pt' );
-	}, 40 ) );
-
-	// Trigger for the initialization.
-	$( window ).trigger( 'resize.stickyNavbar' );
 } );
